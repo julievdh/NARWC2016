da71db4408275305b9e382a6492c02501e7bfbcc
julievdh <julievdh@mit.edu>
2016-10-28 16:12:59 -0400

    Initial Commit

---
+++ MortDensity_subset_NOV_2016.m
@@ -0,0 +1,299 @@
+% Calculates kernel density of distance along coastline of mortalities
+% associated with VS or all COD. 
+
+% Julie van der Hoop jvanderhoop@whoi.edu
+% June 6 2013; Updated 26 Oct 2016 for NARWC 2016
+
+close all; clear all; clc
+
+% load data files
+load beachedMEAS_subset2016.mat
+
+% subtract measurement 2993662 as this is the spatial reference of the tip
+% of the FL peninsula. This way, data are measured from zero.
+% MEAS(:,6) = MEAS(:,6) - 2993662;
+
+% make month fraction vector
+mday = MEAS(:,2)+(MEAS(:,3)/30);
+
+% find vessel strikes
+VS = find(MEAS(:,5) == 2);
+VS = MEAS(VS,:);
+
+% find within dates
+vspre = find(VS(:,1) < 2009);
+vspre4 = [82:109]'; % between 9 Dec 2004 and 9 Dec 2008
+% vspre4 = find(VS(:,1) < 2009 & VS(:,1) > 2004);
+vspost = find(VS(:,1) > 2008);
+
+%% for all OTHER COD
+
+% find COD not = VS
+otherCOD = find(MEAS(:,5) ~= 2);
+otherCOD = MEAS(otherCOD,:);
+
+% find appropriate xi value
+[f_allall,xi_allall,bw] = ksdensity(otherCOD(:,6),'width',200000);
+
+pre = find(otherCOD(:,1) < 2009);
+pre4 = [508:657]'; % between 9 Dec 2004 and 9 Dec 2008
+% pre4 = find(MEAS(:,1) < 2009 & MEAS(:,1) > 2004);
+post = find(otherCOD(:,1) > 2008);
+
+[f_pre_ALL,xi_pre_ALL] = ksdensity(otherCOD(pre,6),xi_allall,'width',200000);
+[f_post_ALL,xi_post_ALL] = ksdensity(otherCOD(post,6),xi_allall,'width',200000);
+[f_pre_ALL4,xi_pre_ALL4] = ksdensity(otherCOD(pre4,6),xi_allall,'width',200000);
+
+figure(1), clf
+set(gcf,'Position',[250 250 785 500])
+plot(xi_allall/1000,f_pre_ALL,'k--'); hold on
+plot(xi_allall/1000,f_pre_ALL4,'k.-'); hold on
+plot(xi_allall/1000,f_post_ALL,'k')
+
+% calculate kernel densities
+[f_post,xi_post] = ksdensity(VS(vspost,6),xi_allall,'width',200000);
+[f_pre,xi_pre] = ksdensity(VS(vspre,6),xi_allall,'width',200000);
+[f_pre4,xi_pre4] = ksdensity(VS(vspre4,6),xi_allall,'width',200000);
+
+% plot VS kernel densities
+plot(xi_pre/1000,f_pre,'r--'); hold on
+plot(xi_pre/1000,f_pre4,'r.-')
+plot(xi_post/1000,f_post,'r')
+ylim([-0.25E-7 1.45E-6])
+xlim([2999 7500])
+
+%% for 2012-2016 events:
+% calculate kernel densities
+[f_post2016,xi_post2016] = ksdensity(MEAS2016(:,6),xi_allall,'width',200000);
+plot(xi_post2016/1000,f_post2016,'b')
+
+% all post (2009 - 2016)
+[f_allpost2016,xi_allpost2016] = ksdensity(vertcat(VS(vspost,6),MEAS2016(:,6)),xi_allall,'width',200000);
+plot(xi_allpost2016/1000,f_allpost2016,'g')
+
+
+%% 
+% rule extents
+plot([3477.270 3698.100],[1.25E-6 1.25E-6],'k') % SEUS
+plot([3698.100 4156.472],[1.25E-6 1.25E-6],'k') % Mid-Atlantic
+plot([4246.695 4338.101],[1.25E-6 1.25E-6],'k') % Morehead City/Beaufort
+plot([4737.100 4807.470],[1.25E-6 1.25E-6],'k') % Chesapeake
+plot([4975.700 5055.800],[1.25E-6 1.25E-6],'k') % Delaware Bay
+plot([5197.324 5292.450],[1.25E-6 1.25E-6],'k') % NY/NJ
+plot([5594.700 5767.630],[1.25E-6 1.25E-6],'k') % BIS
+plot([6032.000 6168.980],[1.25E-6 1.25E-6],'k') % CCB
+plot([5975.760 6247.370],[1.25E-6 1.25E-6],'k') % ORP
+plot([5975.760 6247.370],[1.25E-6 1.25E-6],'k') % GSC
+
+plot([6738.341 7261.435],[1.25E-6 1.25E-6],'k') % BOF
+plot([7337.985 7474.765],[1.25E-6 1.25E-6],'k') % Roseway
+
+
+% add labels
+text(3630,1.28E-6,'SEUS','Rotation',90,'FontSize',12) % SEUS
+text(3950,1.28E-6,'M-A','Rotation',90,'FontSize',12) % Mid-Atlantic
+text(4300,1.28E-6,'MC/B','Rotation',90,'FontSize',12) % Morehead City/Beaufort
+text(4770,1.28E-6,'CB','Rotation',90,'FontSize',12) % Chesapeake
+text(5040,1.28E-6,'DB','Rotation',90,'FontSize',12) % Delaware Bay
+text(5270,1.28E-6,'NY/NJ','Rotation',90,'FontSize',12) % NY/NJ
+text(5650,1.28E-6,'BIS','Rotation',90,'FontSize',12) % Block Island Sound
+text(6350,1.28E-6,'CCB','Rotation',90,'FontSize',12) % Cape Cod Bay
+text(6150,1.28E-6,'ORP/','Rotation',90,'FontSize',12) % Off Race Point
+text(5950,1.28E-6,'GSC/','Rotation',90,'FontSize',12) % Great South Channel
+
+text(7000,1.28E-6,'BOF','Rotation',90,'FontSize',12) % BOF
+text(7400,1.28E-6,'ROS','Rotation',90,'FontSize',12) % Roseway
+
+
+%% legend('Pre-rule All COD','Post-Rule All COD','Pre-rule VS','Post-rule VS','Location','best')
+ylabel('Kernel Density')
+xlabel('Distance along coastline (km)')
+
+% brush plot
+% for i = 1:length(MEAS)
+% plot([MEAS(i,6)/1000 MEAS(i,6)/1000],[11.5E-7 12E-7],'k')
+% end
+%%
+for i = 1:length(VS)
+plot([VS(vspre,6)/1000 VS(vspre,6)/1000],[11E-7 11.5E-7],'r')
+end
+
+for i = 1:length(VS)
+plot([VS(vspost,6)/1000 VS(vspost,6)/1000],[10.5E-7 11E-7],'r')
+end
+
+for i = 1:length(MEAS2016)
+plot([MEAS2016(i,6)/1000 MEAS2016(i,6)/1000],[10.5E-7 11E-7],'b')
+end
+
+set(gca,'XtickLabel',[0:500:4500])
+box off
+plot([7500 7500],[-0.5 1.45E-6],'k')
+plot([0 10000],[1.448E-6 1.448E-6],'k')
+
+set(gcf, 'PaperPositionMode','auto')
+% print -dtiff vanderHoop_Density.tiff
+% print -depsc vanderHoop_CL_Fig4.eps
+
+return
+
+%% US ONLY AND KS TEST 
+
+% find mortalities in US waters
+US = find(MEAS(:,6) < 6745185);
+US = MEAS(US,:);
+
+VSUS = find(US(:,5) == 2);
+VSUS = US(VSUS,:);
+
+% find new pre and post
+pre = find(US(:,1) < 2009);
+post = find(US(:,1) > 2008);
+
+[f_pre_US_ALL,xi_pre_US_ALL] = ksdensity(US(pre,6),'width',200000);
+[f_post_US_ALL,xi_post_US_ALL] = ksdensity(US(post,6),'width',200000);
+
+% find new pre and post
+pre = find(VSUS(:,1) < 2009);
+post = find(VSUS(:,1) > 2008);
+
+[f_pre_VSUS,xi_pre_VSUS] = ksdensity(VSUS(pre,6),'width',200000);
+[f_post_VSUS,xi_post_VSUS] = ksdensity(VSUS(post,6),'width',200000);
+
+plot(xi_post_US_ALL,f_post_US_ALL,'k')
+hold on
+plot(xi_pre_US_ALL,f_pre_US_ALL,'k--')
+plot(xi_pre_VSUS,f_pre_VSUS,'r--')
+plot(xi_post_VSUS,f_post_VSUS,'r')
+
+
+%% 
+figure(3)
+set(gcf,'Position',[1600 -100 800 700])
+subplot(211)
+set(gca,'Position',[0.1300 0.55 0.7750 0.3412])
+
+% plot VS kernel densities
+plot(xi_pre/1000,f_pre,'k--'); hold on
+plot(xi_post/1000,f_post,'k')
+ylim([0 1.28E-6]); xlim([-5 10E3])
+
+legend('1990-2008','2008-2012')
+ylabel('Kernel Density','FontSize',12)
+xlabel('Distance along coastline (km)','FontSize',12)
+
+
+% rule extents
+plot([3562.650 3783.883],[1.2E-6 1.2E-6],'k') % SEUS
+plot([3783.833 4254.585],[1.2E-6 1.2E-6],'k') % Mid-Atlantic
+plot([4358.025 4447.142],[1.2E-6 1.2E-6],'k') % Morehead City/Beaufort
+plot([4854.360 4935.569],[1.2E-6 1.2E-6],'k') % Chesapeake
+plot([5097.220 5178.770],[1.2E-6 1.2E-6],'k') % Delaware Bay
+plot([5322.677 5421.315],[1.2E-6 1.2E-6],'k') % NY/NJ
+plot([5580.100 5911.370],[1.2E-6 1.2E-6],'k') % BIS
+plot([6192.146 6331.840],[1.2E-6 1.2E-6],'k') % CCB
+plot([6133.615 6394.000],[1.2E-6 1.2E-6],'k') % ORP
+plot([5987.500 6394.000],[1.2E-6 1.2E-6],'k') % GSC
+
+text(3E2,1.15E-6,'(a)','FontSize',12)
+
+%% do the same for latitude instead of distance along coast
+
+load VS_LAT % coordsinates of VS, aligned with dates for VS vector
+
+% make month fraction vector
+mday = VS(:,2)+(VS(:,3)/30);
+
+% find within dates
+vspre = find(VS(:,1) < 2009);
+vspost = find(VS(:,1) > 2008);
+
+
+% calculate kernel densities
+[f_pre_LAT,xi_pre] = ksdensity(VS_LAT(vspre));
+[f_post_LAT,xi_post] = ksdensity(VS_LAT(vspost),xi_pre);
+
+% plot VS kernel densities
+figure(2)
+set(gcf,'Position',[1500 -100 800 630])
+plot(xi_pre,f_pre_LAT,'r--')
+hold on
+plot(xi_post,f_post_LAT,'r')
+
+legend('Pre-rule VS','Post-rule VS')
+ylabel('Kernel Density')
+xlabel('Latitude ^oN')
+
+ylim([0 0.27])% plot rule extents
+plot([31.45 29.75],[0.25 0.25],'k') % SEUS
+plot([33.945 31.45],[0.25 0.25],'k') % Mid-Atlantic
+plot([34.462965 34.862133],[0.25 0.25],'k') % Morehead City/Beaufort
+plot([36.701173 37.305429],[0.25 0.25],'k') % Chesapeake
+plot([38.5401883 39.10506],[0.25 0.25],'k') % Delaware Bay
+plot([40.162509 40.5959972],[0.25 0.25],'k') % NY/NJ
+plot([40.5959972 41.3184775],[0.25 0.25],'k') % BIS
+plot([41.699421 42.211725],[0.25 0.25],'k') % CCB
+plot([41.66667 42.5],[0.25 0.25],'k') % ORP
+plot([41.00321 42.5],[0.25 0.25],'k') % GSC
+
+
+figure(3)
+subplot(212)
+
+plot(xi_pre,f_pre_LAT,'k--')
+hold on
+plot(xi_post,f_post_LAT,'k')
+
+% plot rule extents
+plot([31.45 29.75],[0.25 0.25],'k') % SEUS
+plot([33.945 31.45],[0.25 0.25],'k') % Mid-Atlantic
+plot([34.462965 34.862133],[0.25 0.25],'k') % Morehead City/Beaufort
+plot([36.701173 37.305429],[0.25 0.25],'k') % Chesapeake
+plot([38.5401883 39.10506],[0.25 0.25],'k') % Delaware Bay
+plot([40.162509 40.5959972],[0.25 0.25],'k') % NY/NJ
+plot([40.5959972 41.3184775],[0.25 0.25],'k') % BIS
+plot([41.699421 42.211725],[0.25 0.25],'k') % CCB
+plot([41.66667 42.5],[0.25 0.25],'k') % ORP
+plot([41.00321 42.5],[0.25 0.25],'k') % GSC
+
+ylabel('Kernel Density','FontSize',12)
+xlabel('Latitude ^oN','FontSize',12)
+ylim([0 0.27])
+text(21,0.24,'(b)','FontSize',12)
+set(gca,'Position',[0.1300 0.07 0.7750 0.3412])
+
+% add labels to (b)
+text(30.5,0.28,'SEUS','Rotation',90,'FontSize',12)
+text(32,0.28,'M-A','Rotation',90,'FontSize',12)
+text(34.6,0.28,'MC/B','Rotation',90,'FontSize',12)
+text(36.9,0.28,'CB','Rotation',90,'FontSize',12)
+text(38.75,0.28,'DB','Rotation',90,'FontSize',12)
+text(40.3,0.28,'NY/NJ','Rotation',90,'FontSize',12)
+text(41,0.28,'BIS','Rotation',90,'FontSize',12)
+text(41.7,0.28,'GSC','Rotation',90,'FontSize',12)
+text(42.3,0.28,'CCB/ORP','Rotation',90,'FontSize',12)
+
+% add labels to (a)
+text(31.1,0.66,'SEUS','Rotation',90,'FontSize',12)
+text(32,0.66,'M-A','Rotation',90,'FontSize',12)
+text(33.2,0.66,'MC/B','Rotation',90,'FontSize',12)
+text(34.75,0.66,'CB','Rotation',90,'FontSize',12)
+text(35.35,0.66,'DB','Rotation',90,'FontSize',12)
+text(36.1,0.66,'NY/NJ','Rotation',90,'FontSize',12)
+text(37.2,0.66,'BIS','Rotation',90,'FontSize',12)
+text(38.3,0.66,'GSC','Rotation',90,'FontSize',12)
+text(38.9,0.66,'CCB/ORP','Rotation',90,'FontSize',12)
+
+
+%# set size on printed paper
+%#set(gcf, 'PaperUnits','centimeters', 'PaperPosition',[0 0 5 10])
+%# WYSIWYG mode: you need to adjust your screen's DPI (*)
+% set(gcf, 'PaperPositionMode','auto')
+
+%# save as TIFF
+% print -dtiff -r300 SpatialLatDistance.tiff
+
+% print proper dimensioncs for journal
+% set(gcf, 'units', 'centimeters', 'pos', [52 10 17.3 7])
+% set(gcf, 'units', 'centimeters')
+% print -depsc vanderHoop_CL_Fig4.eps
---
+++ NARWC2016.m
@@ -0,0 +1,56 @@
+% NARWC Meeting 2016 - Is the vessel-strike problem solved?
+% 11 Oct 2016
+
+% year / N sightings / Unique IDs / Presumed alive / Survey effort (1000s
+% km) / % Pop seen / All EG Morts / EG EN morts / EG VS Morts / UN morts
+data = [2000,3084,236,342,125,69,2,1,0,1;2001,3848,281,360,127,78,5,1,2,2;
+    2002,2709,303,383,217,79,6,1,1,3;2003,2401,314,396,180,79,2,0,1,0;
+    2004,1804,286,405,259,71,4,1,2,1;2005,3397,352,420,340,84,5,0,2,2;
+    2006,2799,344,431,316,80,5,1,4,0;2007,3736,379,445,267,85,3,1,0,1;
+    2008,4147,388,466,254,83,3,0,0,0;2009,4634,421,483,246,87,5,0,0,4;
+    2010,3221,418,502,263,83,4,3,1,0;2011,3462,435,501,223,87,5,1,1,2;
+    2012,2126,370,500,256,74,4,2,0,2;2013,1905,293,500,212,58,1,1,0,0;
+    2014,2389,361,495,199,72,3,2,0,1;2015,1766,250,NaN,NaN,51,3,0,0,3;
+    2016,NaN,NaN,NaN,NaN,NaN,4,1,1,2];
+
+%% figure 1: How many dead animals we find vs. live animals we see
+
+cd '/Users/julievanderhoop/Documents/MATLAB/NARWC 2016'
+
+figure(1);
+[AX,H1,H2] = plotyy(data(:,1),data(:,6),data(:,1),data(:,7));
+ylabel(AX(2),'All Eg Morts'); ylabel(AX(1),'% Population seen')
+% figure(2)
+% plot(data(:,6),data(:,7),'o')
+% xlabel('Percent of population seen'); ylabel('All Eg Morts')
+% 
+% figure(3)
+% plot(data(:,5),data(:,7),'o')
+% xlabel('Survey Effort'); ylabel('All Eg Morts')
+adjustfigurefont
+print('SightingsMorts.png','-dpng','-r300')
+
+%% Mortality vs SI 
+% Would expect fewer mortalities since rule. Seeing more SI since rule. 
+
+MSI = [2000,1,0,0,0;2001,1,0,1,2;2002,2,0,1,1;2003,1,0,0,1;2004,0,0,1,2;...
+    2005,0,1,0,2;2006,0,1,1,4;2007,0,0,1,0;2008,1,0,0,0;2009,3,0,0,0;...
+    2010,1,0,3,1;2011,5,2,1,1;2012,2,1,2,0;2013,1,0,0,0;2014,7,1,2,0;...
+    2015,NaN,NaN,0,0;2016,NaN,NaN,2,1];
+
+figure(4)
+bar(MSI(:,[4 2]),'stacked') % EN SI and MT
+set(gca,'xtick',1:2:2014,'xticklabel',MSI(1:2:end,1)); title('Entanglement')
+ylim([0 10])
+ylabel('Number of Cases')
+adjustfigurefont
+
+print('Eg_ENMortSI.png','-dpng','-r300')
+
+figure(5)
+bar(MSI(:,[5 3]),'stacked') % VS SI and MT
+set(gca,'xtick',1:2:2014,'xticklabel',MSI(1:2:end,1)); title('Vessel Strikes')
+ylim([0 10])
+ylabel('Number of Cases')
+adjustfigurefont
+print('Eg_VSMortSI.png','-dpng','-r300')
